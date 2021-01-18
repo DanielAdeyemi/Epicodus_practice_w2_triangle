4e946af485de5a51da2c0b989905c0c93943fb65
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-17 19:16:17 -0800

    write checker function v.2 bug: wrong quotes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 function checker(n1, n2, n3) {
-  if (n1 === n2 && n1 === n3) {
+  if ((n1 + n2) <= n3 || (n1 + n3) <= n2 || (n2 + n3) <= n1) {
+    return ("Impossible to make triangle with sides you have provided");
+  } else if (n1 === n2 && n1 === n3) {
     return ("Your sides will make an Equilateral Triagle");
   } else if (n1 === n2 || n2 === n3 || n1 === n3) {
     return ("Your sides will make an Isosceles Triangle");
-  } else if ((n1 + n2) <= n3 || (n1 + n3) <= n2 || (n2 + n3) <= n1) {
-    return ("Impossible to make triangle with sides you have provided");
   } else {
     return ("Your sides will make Scalene Triangle");
   }
@@ -13,10 +13,11 @@ function checker(n1, n2, n3) {
 $(document).ready(function() {
   $('form').submit(function() {
     event.preventDefault();
-    let number1 = parseInt($("#num1").val());
-    let number2 = parseInt($("#num2").val());
-    let number3 = parseInt($("#num3").val());
-    checker(number1, number2, number3);
-    $('#output').text(checker);
+    let number1 = parseInt($('#num1').val());
+    let number2 = parseInt($('#num2').val());
+    let number3 = parseInt($('#num3').val());
+    let check = checker(number1, number2, number3);
+    $('#output').text(check);
+
   });
 })
