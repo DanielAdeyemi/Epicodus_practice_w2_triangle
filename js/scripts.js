bd3b1e871b150081a91080988373f1ae5816d492
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-17 19:07:36 -0800

    write checker function v.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+function checker(n1, n2, n3) {
+  if (n1 === n2 && n1 === n3) {
+    return ("Your sides will make an Equilateral Triagle");
+  } else if (n1 === n2 || n2 === n3 || n1 === n3) {
+    return ("Your sides will make an Isosceles Triangle");
+  } else if ((n1 + n2) <= n3 || (n1 + n3) <= n2 || (n2 + n3) <= n1) {
+    return ("Impossible to make triangle with sides you have provided");
+  } else {
+    return ("Your sides will make Scalene Triangle");
+  }
+}
+
 $(document).ready(function() {
   $('form').submit(function() {
     event.preventDefault();
@@ -5,5 +17,6 @@ $(document).ready(function() {
     let number2 = parseInt($("#num2").val());
     let number3 = parseInt($("#num3").val());
     checker(number1, number2, number3);
+    $('#output').text(checker);
   });
 })
